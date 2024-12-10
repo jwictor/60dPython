5c80fefac7e535e4909c6bbee4dc364ce84fd627
João Wictor <joaowictor@adrosamaster.com>
2024-12-10 15:49:53 -0300

    montando a soma de dois numeros de formas diferentes

---
+++ day2/soma_dois_numeros2.py
@@ -0,0 +1,14 @@
+def somar(numero1, numero2):
+    somar_dois_numeros = numero1 + numero2
+    print(somar_dois_numeros)
+    return somar_dois_numeros
+
+
+def somar_dois_num():
+    numero1 = int(input("Coloque o primeiro numero: "))
+    numero2 = int(input("coloque o segundo numero: "))
+    somar_dois_num = numero1 + numero2
+    print(somar_dois_num)
+    return somar_dois_num
+
+somar_dois_num()
